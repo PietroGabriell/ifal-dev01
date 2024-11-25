ae09a561ce5999cf5f37e89e00df12a48bb97c22
PietroGabriell <pgnn1@aluno.ifal.edu.br>
2024-11-25 15:19:11 -0300

    Add files via upload

---
+++ hello/dict.py
@@ -0,0 +1,5 @@
+alunos = {"Hermione" : "Grifinória\n", "Harry" : "Grifinória\n", "Rony" : "Grifinória\n", "Draco" : "Sonserina\n"}
+
+print("Alunos:\n")
+for aluno in alunos :
+    print(aluno, alunos[aluno])
