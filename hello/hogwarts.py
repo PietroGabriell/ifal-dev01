39eeff44b679ef2d01b425f99d1ffe410f6dc1a0
PietroGabriell <pgnn1@aluno.ifal.edu.br>
2024-11-25 15:01:12 -0300

    Add files via upload

---
+++ hello/hogwarts.py
@@ -0,0 +1,10 @@
+alunos = ["Hermione" , "Harry" , "Rony"]
+professores = ["Bob Esponja" , "Patrick" , "Lula Molusco" , "Sr.Sirigueijo"]
+
+print("Alunos: \n")
+for i in range(len(alunos)):
+    print((i + 1) ,alunos[i])
+
+print("\nProfessores:\n")
+for i in range(len(professores)):
+    print((i + 1) ,professores[i])
