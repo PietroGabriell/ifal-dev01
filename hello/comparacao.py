6ca378995bc03717edf015c0f4eeaca69ad87148
PietroGabriell <pgnn1@aluno.ifal.edu.br>
2024-11-25 13:46:24 -0300

    Add files via upload

--- hello/comparacao.py
+++ hello/comparacao.py
@@ -1,15 +1,15 @@
 def main():
-    x = int(input("Escolha o seu primeiro número"))
-    y = int(input("Escolha o seu segundo número"))
+    x = int(input("Escolha o seu primeiro número\n \t"))
+    y = int(input("Escolha o seu segundo número\n \t"))
     verificacao(x,y)
 
 
 
 def verificacao(x,y):
     if x < y:
-        print(f" {x} é maior que {y}")
+        print(f" {x} é menor que {y}")
     elif x > y:
-        print(f"{x} é menor que {y}")
+        print(f"{x} é maior que {y}")
     else:
         print(f"{x} é igual a {y}")
 
